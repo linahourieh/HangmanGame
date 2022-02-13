dd30aecd9b3057157528109cf8a18beae812bdc5
LynnHourieh <93599067+LynnHourieh@users.noreply.github.com>
2022-02-13 13:30:16 +0200

    update my version

--- choices.py
+++ choices.py
@@ -1,81 +1,61 @@
 from MergeSort import mergeSort
 from BinarySearch import binarySearch
-from OptionScreen import Loading_function, color
 import time
+from OptionScreen import display_options
+
 
 
 def choice_2_b(our_dictionary):
-    Loading_function()
     list_of_keys = list(our_dictionary.keys())
     if len(list_of_keys) != 0:
         mergeSort(list_of_keys)
         print('Available list: ')
-        print(color.PURPLE + str(list_of_keys) + color.END)
+        print(list_of_keys)
     else:
         print("Sorry! the list of words is empty")
     time.sleep(3)
 
 
+
+
 def choice_3_b(our_dictionary):
-    Loading_function()
+    # add a new word
     key = input("Add new Word: ")
     value = input("Add an appropriate hint: ")
     new_dictionary = {key: value}
     our_dictionary.update(new_dictionary)
-    Loading_function()
-    time.sleep(1)
-
+    newList = list(our_dictionary.keys())
+    global sorted_list
+    sorted_list = mergeSort(newList)
+    time.sleep(2)
 
 def choice_4_b(our_dictionary):
-    Loading_function()
     element = input("Enter a word to remove it from dictionary: ")
-    list_key = list(our_dictionary.keys())
-    mergeSort(list_key)
-
-    result = binarySearch(list_key, 0, len(list_key) - 1, element)
-
-    Loading_function()
-    if result:
-        del our_dictionary[element]
-    else:
-        print("Word not Found")
-
-    time.sleep(1)
-
-
-def choice_5_b(our_dictionary):
-    Loading_function()
-    element = input("Enter a word you wish to change its hint: ")
-    modified_hint = input("Enter a modified hint: ")
-
-    Loading_function()
     list_key = list(our_dictionary.keys())
     mergeSort(list_key)
 
     x = binarySearch(list_key, 0, len(list_key) - 1, element)
 
-    if x:
-        our_dictionary[element] = modified_hint
+    if x == True:
+        del our_dictionary[element]
     else:
         print("Word not Found")
 
-    time.sleep(1)
+    time.sleep(3)
 
+def choice_5_b(our_dictionary):
+    element = input("Enter a word you wish to change its hint: ")
+    modified_hint = input("Enter a modified hint: ")
 
-def choice_7_b(our_dictionary, dic_path):
-    wish = input('do you wish to save the changes [y/n]? ')
-    if wish == 'y':
-        our_dictionary_file_0 = open(dic_path, 'w')
-        for k, val in our_dictionary.items():
-            our_dictionary_file_0.write(k + ':' + val + '\n')
-        time.sleep(0.7)
-        print(color.PURPLE + 'ok...' + color.END)
-        time.sleep(0.7)
-        print(color.PURPLE + 'Goodbye 😴' + color.END)
-        time.sleep(0.7)
+    list_key = list(our_dictionary.keys())
+    mergeSort(list_key)
+
+    x = binarySearch(list_key, 0, len(list_key) - 1, element)
+
+    if x == True:
+        our_dictionary[element] = modified_hint
+        print(our_dictionary)
     else:
-        time.sleep(0.7)
-        print(color.PURPLE + 'Your loss...' + color.END)
-        time.sleep(0.7)
-        print(color.PURPLE + 'Goodbye 😴' + color.END)
-        time.sleep(0.7)
+        print("Word not Found")
+
+    time.sleep(3)
